8980bd75fa3588cc90ba091a4cd0015c35e5c040
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-04-02 17:33:12 +0530

    Add more features

--- Responses.py
+++ Responses.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def sample_responses(input_text):
+def respond_to_message(input_text):
 
     user_message = str(input_text).lower()
 
@@ -11,11 +11,20 @@ def sample_responses(input_text):
     if user_message in ("who are you", "who are you?"):
         return "I am ChatMate."
 
-    if user_message in ("time", "time?"):
+    if "time" in user_message:
         now = datetime.now()
         date_time = now.strftime("%d/%m/%y, %H:%M:%S")
 
         return str(date_time)
+
+    if "date" in user_message:
+        now = datetime.now()
+        date_time = now.strftime("%d/%m/%y")
+
+        return str(date_time)
+
+    if "name" in user_message:
+        return "My name is ChatMate."
 
     # If none of the above conditions are met, return this
     return "I don't understand you."
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def help_command(update, context):
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
-    response = R.sample_responses(text)
+    response = R.respond_to_message(text)
 
     update.message.reply_text(response)
 
